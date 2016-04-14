86f3162401df7265ae514d816c351244073594dd
yko <ykorshak@gmail.com>
2016-04-14 23:02:23 +0200

    initial commit

---
+++ demo.js
@@ -0,0 +1,40 @@
+var ctx;
+if (canvas.getContext) {
+  ctx = canvas.getContext('2d');
+} else
+  throw "This browser doesn't support canvas.getContext :(";
+
+var board = new AiBoard(ctx);
+
+function redAi(board) {
+   return { action: 'move', x: this.x+1, y: this.y };
+}
+
+function blueAi(board) {
+   return { action: 'move', x: this.x, y: this.y-1 };
+}
+
+board.addUnit({x: 1, y: 1, color: 'red', ai: redAi });
+board.addUnit({x: 8, y: 8, color: 'blue', ai: blueAi });
+
+board.redraw();
+
+step.addEventListener("click", function() {
+  board.oneRound();
+  board.redraw();
+});
+
+var interval_id;
+run.addEventListener("click", function() {
+  if (interval_id) {
+    clearInterval(interval_id);
+    interval_id = undefined;
+    run.value = "Run";
+  } else {
+    interval_id = setInterval(function() {
+      board.oneRound();
+      board.redraw();
+    }, 1000);
+    run.value = "Stop";
+  }
+});
